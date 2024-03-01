ac9796f496488bb0e7145fd0d64fbd2378fcc733
baldwisj <baldwisj@gmail.com>
2024-03-01 07:35:33 -0500

    Added additional code to index.js

--- db/schema.sql
+++ db/schema.sql
@@ -4,17 +4,16 @@ CREATE DATABASE employee_db;
 use employee_db;
 
 CREATE TABLE department(
-    id INT NIT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role(
-    id INT NOT NULL INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
-    salary Decimal,
+    salary Decimal(10,2),
     department_id INT,
-    FOREIGN KEY (department_id),
-    References department(id)
+    FOREIGN KEY (department_id) REFERENCES department(id)
 
 );
 
@@ -24,8 +23,6 @@ CREATE TABLE employee(
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    REFERENCES role(id),
-    REFERENCES employee(id),
-    FOREIGN KEY (manager_id),
-    FOREIGN KEY (role_id)
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
