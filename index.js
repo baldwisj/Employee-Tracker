98a5bf50c030a570955c6021b2ccc881be8cda90
baldwisj <baldwisj@gmail.com>
2024-03-06 21:12:53 -0500

    Added comments and modified a few issues

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2/promise')
 
 require("dotenv").config();
-
+//Call my environmental variables
 const dbName = process.env.DB_NAME;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
-
-async function dbEmployee() {
+//This calls the dbEmployee function when the app is initiated
+async function dbEmployee(userAction) {
     try {
         const db = await mysql.createConnection({
             host: "localhost",
@@ -19,13 +19,15 @@ async function dbEmployee() {
 
         let dbRows = [];
         let userOutput = [];
-
-        switch (select) {
+        // This starts the switch case for the user to select an action
+        switch (userAction) {
+            //This displays all the departments and the information included in that
             case "View departments":
                 dbRows = await db.query("SELECT * FROM department");
                 console.table(dbRows[0]);
                 break;
 
+            //This displays all the roles and the information included in that
             case "View roles":
                 dbRows = await db.query(`
                 SELECT
@@ -40,7 +42,7 @@ async function dbEmployee() {
                 break;
 
 
-
+            //This displays all the employees and the information included in each
             case "View employees":
                 dbRows = await db.query(`
                 SELECT
@@ -57,7 +59,7 @@ async function dbEmployee() {
                 `)
                 console.table(dbRows[0]);
                 break;
-
+            //This allows the user to add a department by inputing the user output into the department
             case "Add department":
                 userOutput = await inquirer.prompt([
                     {
@@ -76,20 +78,20 @@ async function dbEmployee() {
                 break;
 
 
-
+            //This allows the user to add a role by entering a role title, salary for the position, and department
             case "Add role":
 
                 userOutput = await inquirer.prompt([
                     {
-                        name: "roleName",
+                        name: "title",
                         message: "Enter New Role:",
                     },
                     {
-                        name: "roleSalary",
+                        name: "salary",
                         message: "Enter Role Salary:",
                     },
                     {
-                        name: "roleDpt",
+                        name: "titleDpt",
                         message: "Enter Role Department:",
                     },
                 ]);
@@ -98,9 +100,9 @@ async function dbEmployee() {
 
                 const { title, salary, titleDept } = userOutput;
 
-
+                //This selects an id from the department title
                 const deptId = await db.query(
-                    `SELECT IF NULL((SELECT id FROM department WHERE name = "${titleDept}"), "Department Does Not Exist")`
+                    `SELECT COALESCE((SELECT id FROM department WHERE name = "${titleDept}"), "Department Does Not Exist")`
                 );
 
 
@@ -113,7 +115,7 @@ async function dbEmployee() {
                     break;
                 }
 
-
+                //this inserts the user input into the role table
                 dbRows = await db.query(
                     ` INSERT INTO role (title, salary, department_id) VALUES ('${title}', '${salary}', '${department_id}');`
                 );
@@ -121,7 +123,7 @@ async function dbEmployee() {
 
                 break;
 
-
+            //This allows the user to add a new employee by entering a name, role and manager
             case "Add employee":
                 userOutput = await inquirer.prompt([
                     {
@@ -150,42 +152,54 @@ async function dbEmployee() {
                 );
 
                 const { first_name, last_name, role, manager } = userOutput;
+                // This assigns the inital index of roles to the role_data variable
                 const role_data = allRoles[0].filter((rol) => {
                     return rol.title === role;
                 });
-
+                // This assigns the inital index of managers to manager_data variable
                 const manager_data = allManagers[0].filter((man) => {
                     return `${man.first_name} ${man.last_name}` === manager;
                 });
 
+                if (role_data.length === 0) {
+                    console.log("Role not found. Please enter a valid role.");
+                    break;
+                }
+
+                if (manager_data.length === 0) {
+                    console.log("Manager not found. Please enter a valid manager.");
+                    break;
+                }
+                //This inserts the new data into the employee table
                 dbRows = await db.query(
                     `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${first_name}', '${last_name}', ${role_data[0].id}, ${manager_data[0].id})`
                 );
 
                 break;
 
-
+            //this case allows the user to change the employee title
             case "Change Employee title":
+                //This grabs the data from the employee table
                 existingEmployees = await db.query(`
                                 SELECT id, first_name, last_name FROM employee;`);
-
+                //This grabs the existing titles from the role table
                 existingTitles = await db.query(`
                                 SELECT id, title FROM role;`);
-
+                //This iterates over each instance of the employee array and displays their name and employee id
                 const allEmployees = existingEmployees[0].map((employee) => {
                     return {
                         name: `${employee["first_name"]} ${employee.last_name}`,
                         value: employee.id,
                     };
                 });
-
+                //This iterates over each instance of the role array and displays the title name and role id
                 const titles = existingTitles[0].map((role) => {
                     return {
                         name: role.title,
                         value: role.id,
                     };
                 });
-
+                //The following allows the user to choose an employee and title
                 userOutput = await inquirer.prompt([
                     {
                         type: "list",
@@ -223,7 +237,7 @@ function userPrompt() {
     inquirer.prompt([
         {
             type: "list",
-            name: "select",
+            name: "action",
             message: "Please select an action",
             choices: [
                 "View departments",
@@ -239,8 +253,8 @@ function userPrompt() {
         },
     ])
         .then(async (res) => {
-            await dbEmployee(res.select);
-            res.select === "Quit" ? process.exit() : userPrompt();
+            await dbEmployee(res.action);
+            res.action === "Quit" ? process.exit() : userPrompt();
         })
         .catch((err) => {
             //Check if the error is related to the terminal
